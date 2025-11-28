4cf5dfdc7bc9531dcf8236f8816b0016b2d00f8a
mshilacodes <mshaisonia@gmail.com>
2025-11-28 23:49:49 +0300

    Updated def fucntion in predict.py

--- model/prediction.py
+++ model/prediction.py
@@ -3,22 +3,10 @@ import numpy as np
 import os
 
 def predict(data):
-
-    model_path = os.path.join(os.path.dirname(__file__), "rf_classifier_model.pkl")
    
     #load model
     with open("model/rf_classifier_model.pkl", "rb") as f:
         clf=pickle.load(f)
 
-    X = np.array(data, dtype=float)
-
-    try:
-        return clf.predict(X)
-    except AttributeError:
-
-        if hasattr(clf, 'named_steps'):
-            final_estimator = clf.named_steps.get('classifier') or clf.named_steps.get('model')
-            if final_estimator:
-                return final_estimator.predict(X)
-        raise
+    return clf.predict(np.array(data, dtype=float))
 
