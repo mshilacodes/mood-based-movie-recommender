4d5d0f597b4cee8315308036bc2e9201a654f7b7
mshilacodes <mshaisonia@gmail.com>
2025-11-28 23:16:56 +0300

    Added encoder to prediction.py

--- model/prediction.py
+++ model/prediction.py
@@ -1,6 +1,15 @@
 import pickle
+import numpy as np
 
 def predict(data):
+    #load model
     with open("model/rf_classifier_model.pkl", "rb") as f:
         clf=pickle.load(f)
-    return clf.predict(data)
+
+    with open("model/rf_classifier_model.pkl", "rb") as f:
+        le=pickle.load(f)
+
+    encoded = le.transform(data)
+    encoded = np.array(encoded).reshape(-1,1)
+
+    return clf.predict(encoded)
