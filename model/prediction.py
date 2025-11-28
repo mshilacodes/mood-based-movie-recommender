89f6f3202dbb02276819d352bebba90272abdbf4
mshilacodes <mshaisonia@gmail.com>
2025-11-28 23:46:27 +0300

    changing name of saved model

--- model/prediction.py
+++ model/prediction.py
@@ -5,10 +5,20 @@ import os
 def predict(data):
 
     model_path = os.path.join(os.path.dirname(__file__), "rf_classifier_model.pkl")
+   
     #load model
     with open("model/rf_classifier_model.pkl", "rb") as f:
         clf=pickle.load(f)
 
     X = np.array(data, dtype=float)
 
-    return clf.predict(X)
+    try:
+        return clf.predict(X)
+    except AttributeError:
+
+        if hasattr(clf, 'named_steps'):
+            final_estimator = clf.named_steps.get('classifier') or clf.named_steps.get('model')
+            if final_estimator:
+                return final_estimator.predict(X)
+        raise
+
