b6424e5635ce11441517cf66f57f629d41e67cde
mshilacodes <mshaisonia@gmail.com>
2025-11-29 01:17:06 +0300

    Updated prediction.py

--- model/prediction.py
+++ model/prediction.py
@@ -4,45 +4,27 @@ import pandas as pd
 import os
 
 MODEL_PATH = "model/rf_classifier_model.pkl"
-IMPUTER_PATH = "model/imputer.pkl"
 COLS_PATH = "model/columns.pkl"
 KEYWORD_PATH = "model/keyword_cols.pkl"
 
 
-with open(MODEL_PATH, "rb") as f:
-    clf = pickle.load(f)
-
-with open(IMPUTER_PATH, "rb") as f:
-    imputer = pickle.load(f)
-
-with open(COLS_PATH, "rb") as f:
-    training_cols = pickle.load(f)
-
-with open(KEYWORD_PATH, "rb") as f:
-    keyword_cols = pickle.load(f)
-
-
-
-
 
 def predict(data):
+    with open(MODEL_PATH, "rb") as f:
+        clf = pickle.load(f)
 
-    encoded_emotion = int(data[0][0])
+    with open(COLS_PATH, "rb") as f:
+        training_cols = pickle.load(f)
 
-    df = pd.DataFrame([{
-
-        "emotions": encoded_emotion
-    }])
-
-    df = df.reindex(columns=training_cols, fill_value=0)
-
-    for col in keyword_cols:
-        if col in df.columns:
-            df[col] = df[col]*10
-    
-    df_imputed = imputer.transform(df)
-
-    prediction = clf.predict(df_imputed)
+    with open(KEYWORD_PATH, "rb") as f:
+        keyword_cols = pickle.load(f)
 
 
-    return prediction
+    X = pd.DataFrame(np.zeros((1,len(training_cols))), columns=training_cols)
+
+    if "emotion" in X.columns:
+        X.loc[0, "emotion"] = emotion_code
+    else:
+        raise ValueError("Column 'emotion' was not in training data.")
+
+    return clf.predict(X)[0]
