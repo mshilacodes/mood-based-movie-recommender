6c65261f3b1fbe0ae03c92dcca2c1f6860d175c6
mshilacodes <mshaisonia@gmail.com>
2025-11-29 00:38:20 +0300

    Moved Keyword.pkl to model folder. Edited predict.py

--- model/prediction.py
+++ model/prediction.py
@@ -1,12 +1,46 @@
 import pickle
 import numpy as np
+import pandas as pd
 import os
 
+MODEL_PATH = "model/rf_classifier_model.pkl"
+IMPUTER_PATH = "model/imputer.pkl"
+COLS_PATH = "model/columns.pkl"
+KEYWORD_PATH = "model/keyword_cols.pkl"
+
+
+with open(MODEL_PATH, "rb") as f:
+    clf = pickle.load(f)
+
+with open(IMPUTER_PATH, "rb") as f:
+    imputer = pickle.load(f)
+
+with open(COLS_PATH, "rb") as f:
+    training_cols = pickle.load(f)
+
+with open(KEYWORD_PATH, "rb") as f:
+    keyword_cols = pickle.load(f)
+
+
+
+
+
 def predict(data):
-   
-    #load model
-    with open("model/rf_classifier_model.pkl", "rb") as f:
-        clf=pickle.load(f)
 
-    return clf.predict(data)
+    encoded_emotion = data[0][0]
 
+    df = pd.DataFrame([{
+
+        "emotion_encoded": encoded_emotion
+    }])
+
+    df = df.reindex(columns=training_cols, fill_value=0)
+
+    for col in keyword_cols:
+        if col in df.columns:
+            df[col] = df[col]*10
+    
+    df_imputed = imputer.transform(df)
+
+
+    return clf.predict(df_imputed)
