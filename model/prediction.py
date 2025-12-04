e0e1425cb28dd2d28bdff6cadd89e60f089a6028
mshilacodes <mshaisonia@gmail.com>
2025-12-04 17:50:02 +0300

    Created preprocessing pkl and combined with model in predict.py

--- model/prediction.py
+++ model/prediction.py
@@ -4,6 +4,16 @@ import pandas as pd
 import os
 from sklearn.pipeline import Pipeline
 
+with open("model/preprocessing.pkl", "rb") as f:
+    preprocessing = pickle.load(f)
+
+with open("model/model_pipe.pkl", "rb") as f:
+    model = pickle.load(f)
+
+pipeline = Pipeline([
+    ("preprocess", preprocessing),
+    ("model", model)
+])    
 
 def predict(emotion, year_min, year_max):
     input_df = pd.DataFrame([{
@@ -12,4 +22,4 @@ def predict(emotion, year_min, year_max):
         "year_max": year_max
     }])
 
-    return Pipeline.predict(input_df)
+    return pipeline.predict(input_df)
