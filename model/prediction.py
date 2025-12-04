71c3e0976149c3d71573f036cd249afdc46e789d
mshilacodes <mshaisonia@gmail.com>
2025-12-04 17:27:19 +0300

    Debugged inedx-cop.ipynb, deleted unnecessary pickle files . Edited prediction.py

--- model/prediction.py
+++ model/prediction.py
@@ -2,62 +2,14 @@ import pickle
 import numpy as np
 import pandas as pd
 import os
+from sklearn import Pipeline
 
 
-MODEL_PATH = "model/rf_classifier_model.pkl"
-IMPUTER_PATH = "model/imputer.pkl"
-COLS_PATH = "model/columns.pkl"
-KEYWORD_PATH = "model/keyword_cols.pkl"
-TFIDF_PATH = "model/tfidf.pkl"
-EMOTION_PATH = "model/emotions.pkl"
-NUM_PATH = "model/num_features.pkl"
+def predict(emotion, year_min, year_max):
+    input_df = pd.DataFrame([{
+        "emotion": emotion,
+        "year_min": year_min,
+        "year_max": year_max
+    }])
 
-
-with open(MODEL_PATH, "rb") as f:
-        clf = pickle.load(f)
-
-
-with open(COLS_PATH, "rb") as f:
-      training_cols=pickle.load(f)
-
-with open(KEYWORD_PATH, "rb") as f:
-      keyword_cols = pickle.load(f) 
-
-with open(IMPUTER_PATH, "rb") as f:
-      imputer = pickle.load(f)
-
-with open(TFIDF_PATH, 'rb') as f:
-      tfidf =pickle.load(f)
-
-with open(EMOTION_PATH, "rb") as f:
-      emotion_encoder = pickle.load(f)
-
-with open(NUM_PATH, 'rb') as f:
-      num_features = pickle.load(f)
-                       
-print("Loaded training columns:", len(training_cols))
-
-
-def predict(data):
-
-    emotion,year_min,year_max = data
-
-    X = pd.DataFrame(np.zeros((1, len(training_cols))), columns=training_cols)
-    
-    if "emotion" in X.columns:
-        X["emotion"] = emotion
-
-    if "year_min" in X.columns:
-        X["year_min"] = year_min
-
-    if "year_max" in X.columns:
-        X["year_max"] = year_max
-
-    for col in keyword_cols:
-        if col in X.columns:
-            X[col] = 0
-
-    X[num_features] = imputer.transform(X[num_features])
-
-    # Predict
-    return clf.predict(X)
+    return Pipeline.predict(input_df)
